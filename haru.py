2b25a247977f4399a4922531095f83f5b361e5b0
pikdum <pikdum@kuudere.moe>
2022-02-05 02:08:26 -0600

    feat: basic subsplease.org support

---
+++ haru.py
@@ -0,0 +1,151 @@
+#!/usr/bin/env python3
+import sys
+from urllib.parse import urlencode, parse_qsl
+import xbmc
+import xbmcgui
+import xbmcplugin
+import requests
+import resolveurl
+from bs4 import BeautifulSoup
+
+_URL = sys.argv[0]
+_HANDLE = int(sys.argv[1])
+
+
+def log(x):
+    xbmc.log("[HARU] " + x, xbmc.LOGINFO)
+
+
+def get_url(**kwargs):
+    return "{}?{}".format(_URL, urlencode(kwargs))
+
+
+def show_main_menu():
+    xbmcplugin.setPluginCategory(_HANDLE, "Main Menu")
+    xbmcplugin.setContent(_HANDLE, "videos")
+
+    list_item = xbmcgui.ListItem(label="Subsplease - All")
+    is_folder = True
+    xbmcplugin.addDirectoryItem(
+        _HANDLE, get_url(action="subsplease_all"), list_item, is_folder
+    )
+
+    list_item = xbmcgui.ListItem(label="ResolveURL Settings")
+    is_folder = False
+    xbmcplugin.addDirectoryItem(
+        _HANDLE, get_url(action="resolveurl_settings"), list_item, is_folder
+    )
+
+    xbmcplugin.addSortMethod(_HANDLE, xbmcplugin.SORT_METHOD_LABEL_IGNORE_THE)
+    xbmcplugin.endOfDirectory(_HANDLE)
+
+
+def show_subsplease_all():
+    xbmcplugin.setPluginCategory(_HANDLE, "Subsplease - All")
+
+    page = requests.get("https://subsplease.org/shows/")
+    soup = BeautifulSoup(page.text, "html.parser")
+    links = filter(lambda x: x["href"].startswith("/shows/"), soup.find_all("a"))
+
+    for link in links:
+        list_item = xbmcgui.ListItem(label=link["title"])
+        is_folder = True
+        url = get_url(
+            action="subsplease_show", url="https://subsplease.org" + link["href"]
+        )
+        xbmcplugin.addDirectoryItem(_HANDLE, url, list_item, is_folder)
+
+    xbmcplugin.addSortMethod(_HANDLE, xbmcplugin.SORT_METHOD_LABEL_IGNORE_THE)
+    xbmcplugin.endOfDirectory(_HANDLE)
+
+
+def show_subsplease_show(url):
+    page = requests.get(url)
+    soup = BeautifulSoup(page.text, "html.parser")
+    sid = soup.find(id="show-release-table")["sid"]
+    log(f"{sid=}")
+    show_title = soup.find("h1", class_="entry-title").text
+    log(f"{show_title=}")
+
+    xbmcplugin.setPluginCategory(_HANDLE, show_title)
+
+    episodes = requests.get(
+        f"https://subsplease.org/api/?f=show&tz=America/Chicago&sid={sid}"
+    ).json()
+
+    if episodes["episode"]:
+        for episode, episode_info in episodes["episode"].items():
+            list_item = xbmcgui.ListItem(label=episode)
+            list_item.setInfo(
+                "video",
+                {"title": episode, "genre": "Anime", "mediatype": "video"},
+            )
+            list_item.setProperty("IsPlayable", "true")
+            hq_download = episode_info["downloads"][-1]
+            is_folder = False
+            magnet = get_nyaa_magnet(hq_download["torrent"])
+            url = get_url(action="play_magnet", magnet=magnet)
+            xbmcplugin.addDirectoryItem(_HANDLE, url, list_item, is_folder)
+
+    # TODO: get a working batch poc
+    if episodes["batch"]:
+        for batch, batch_info in episodes["batch"].items():
+            list_item = xbmcgui.ListItem(label=batch)
+            hq_download = batch_info["downloads"][-1]
+            is_folder = True
+            url = get_url(
+                action="subsplease_batch",
+                batch=batch,
+                batch_torrent=hq_download["torrent"],
+            )
+            xbmcplugin.addDirectoryItem(_HANDLE, url, list_item, is_folder)
+
+    xbmcplugin.addSortMethod(_HANDLE, xbmcplugin.SORT_METHOD_LABEL_IGNORE_THE)
+    xbmcplugin.endOfDirectory(_HANDLE)
+
+
+def show_subsplease_batch(batch, batch_torrent):
+    xbmcplugin.setPluginCategory(_HANDLE, batch)
+    magnet = get_nyaa_magnet(batch_torrent)
+    url = resolveurl.HostedMediaFile(url=magnet).resolve()
+    log(f"{url=}")
+
+    xbmcplugin.addSortMethod(_HANDLE, xbmcplugin.SORT_METHOD_LABEL_IGNORE_THE)
+    xbmcplugin.endOfDirectory(_HANDLE)
+
+
+def get_nyaa_magnet(url):
+    page = requests.get(url.replace("/torrent", ""))
+    soup = BeautifulSoup(page.text, "html.parser")
+    magnet = soup.find("a", class_="card-footer-item").get("href")
+    return magnet
+
+
+def play_magnet(magnet):
+    resolved_url = resolveurl.HostedMediaFile(url=magnet).resolve()
+    play_item = xbmcgui.ListItem(path=resolved_url)
+    xbmcplugin.setResolvedUrl(_HANDLE, True, listitem=play_item)
+
+
+def router(paramstring):
+    params = dict(parse_qsl(paramstring))
+
+    if params:
+        if params["action"] == "play_magnet":
+            play_magnet(params["magnet"])
+        elif params["action"] == "subsplease_all":
+            show_subsplease_all()
+        elif params["action"] == "subsplease_show":
+            show_subsplease_show(params["url"])
+        elif params["action"] == "subsplease_batch":
+            show_subsplease_batch(params["batch"], params["batch_torrent"])
+        elif params["action"] == "resolveurl_settings":
+            resolveurl.display_settings()
+        else:
+            raise ValueError("Invalid paramstring: {}!".format(paramstring))
+    else:
+        show_main_menu()
+
+
+if __name__ == "__main__":
+    router(sys.argv[2][1:])

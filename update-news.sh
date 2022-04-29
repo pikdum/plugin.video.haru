c0af475b24aaf702332d6dece6f939b73eecc203
pikdum <pikdum@kuudere.moe>
2022-04-28 21:33:57 -0500

    fix: remove news formatting

--- update-news.sh
+++ update-news.sh
@@ -3,5 +3,6 @@ set -euo pipefail
 
 COMMIT_HASH="$(git log -n 2 --pretty=format:%H -- CHANGELOG.md | tail -n+2)"
 CHANGES="$(diff --changed-group-format='%>' --unchanged-group-format='' <(git show "$COMMIT_HASH":CHANGELOG.md) CHANGELOG.md || true)"
+CHANGES_FORMATTED="$(echo "$CHANGES" | pandoc --from markdown --to plain)"
 
-xmlstarlet ed --inplace -u "//news" -v "$CHANGES" addon.xml
+xmlstarlet ed --inplace -u "//news" -v "$CHANGES_FORMATTED" addon.xml

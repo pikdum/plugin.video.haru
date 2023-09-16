617737158ce9bd9d8695db31bc559c0bb76f27f4
pikdum <pikdum@users.noreply.github.com>
2023-09-16 04:30:58 -0500

    feat: better poster + fanart (#37)
    * wip: poc of scraping images
    
    * feat: poster + fanart from mona

--- resources/lib/util.py
+++ resources/lib/util.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
+import re
 import sys
-from urllib.parse import urlencode
+from urllib.parse import urlencode, quote
 
 import xbmc
 import xbmcaddon
@@ -10,6 +11,7 @@ from resolveurl.lib import kodi
 _URL = sys.argv[0]
 HANDLE = int(sys.argv[1])
 VIDEO_FORMATS = list(filter(None, kodi.supported_video_extensions()))
+MONA_URL = "https://wild-fire-3987.fly.dev"
 
 
 def get_setting(setting, default=None):
@@ -24,19 +26,35 @@ def get_url(**kwargs):
     return "{}?{}".format(_URL, urlencode(kwargs))
 
 
-def set_art(list_item, artwork_url):
-    if artwork_url:
-        list_item.setArt({"poster": artwork_url, "thumb": artwork_url})
+def set_show_art(list_item, title):
+    poster = f"{MONA_URL}/poster/show/{quote(title)}"
+    fanart = f"{MONA_URL}/fanart/show/{quote(title)}"
+    list_item.setArt({"poster": poster, "thumb": poster, "fanart": fanart})
+
+
+def set_torrent_art(list_item, name):
+    pattern = r"\[(.+)?\] (.+?) (S\d* )?- (\d+)"
+    match = re.match(pattern, name)
+    if match and match.group(2):
+        set_show_art(list_item, match.group(2))
 
 
 def slugify(text):
-    return (
-        text.lower()
-        .replace(" ", "-")
-        .replace(",", "")
-        .replace("!", "")
-        .replace("+", "")
-    )
+    # lowercase
+    text = text.lower()
+    # strip bbcode
+    text = re.sub(r"\[.*?\]", "", text)
+    # remove parens
+    text = text.replace("(", "").replace(")", "")
+    # remove apostrophes of all sorts
+    text = text.replace("'", "").replace("’", "")
+    # remove whatever this is
+    text = text.replace("+", "").replace("@", "")
+    # replace non-alphanumeric with dashes
+    text = re.sub(r"[^a-zA-Z0-9_]+", "-", text)
+    # strip leading and trailing dashes
+    text = text.strip("-")
+    return text
 
 
 def open_settings(addon_id):
